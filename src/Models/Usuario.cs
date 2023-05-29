fb73cb2c5af6aca8c5e3a6ddd4cd1c12a0f6329b
Lucas Russo <lukasrusso22@gmail.com>
2023-05-28 23:59:05 -0300

    creating User model

---
+++ src/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace src.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar o nome")]
+
+        public string Nome { get; set; }  
+       
+        public string Email { get; set; }
+        public string DescricaoPerfil { get; set; }
+        public IFormFile Foto { get; set; }
+
+    }
+}
