1ae4407a70f2a5bc5f20080bc070edd4489404ea
Lucas Russo <lukasrusso22@gmail.com>
2023-06-20 22:44:57 -0300

    adding azure auth

--- src/Startup.cs
+++ src/Startup.cs
@@ -26,7 +26,7 @@ namespace src
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {            
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
